f8cd9bc86418a9a5ccb2271e05b779a5d1c96fe7
Josh Yolles <joshyolles@gmail.com>
2019-08-01 05:25:28 -0700

    Add files via upload

---
+++ improved-bike-falls/__resource.lua
@@ -0,0 +1,7 @@
+--# Converted to a FiveM script by minipunch, OG from someone at gta5-mods.com
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    "improved-bike-falls/cl_bike-fall-realism.lua"
+}
